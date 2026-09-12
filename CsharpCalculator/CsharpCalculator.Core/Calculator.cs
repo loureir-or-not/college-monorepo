4c576c5e4f20ae4395f697f927592ec287fb5576
Renato Loureiro <loureir.or.not@gmail.com>
2026-09-12 03:05:01 +0100

    Initialise a basic calculator library

---
+++ CsharpCalculator/CsharpCalculator.Core/Calculator.cs
@@ -0,0 +1,48 @@
+namespace CsharpCalculator.Core;
+
+enum CalculatorOp
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Power,
+}
+
+class Calculator {
+    public static int Calculate(CalculatorOp[] ops, int[] nums, out int result)
+    {
+        if (ops.Length >= nums.Length)
+        {
+            result = 0;
+            return 1;
+        }
+        result = nums[0];
+        for (int i = 0; i < ops.Length; i++)
+        {
+            switch (ops[i]) {
+                case CalculatorOp.Add:
+                    result += nums[i+1];
+                    break;
+                case CalculatorOp.Subtract:
+                    result -= nums[i+1];
+                    break;
+                case CalculatorOp.Multiply:
+                    result *= nums[i+1];
+                    break;
+                case CalculatorOp.Divide:
+                    if (nums[i+1] == 0)
+                    {
+                        return 1;
+                    } else {
+                        result /= nums[i+1];
+                    }
+                    break;
+                case CalculatorOp.Power:
+                    result = (int)Math.Pow(result, nums[i+1]);
+                    break;
+            }
+        }
+        return 0;
+    }
+}
---
+++ CsharpCalculator/CsharpCalculator.Webapp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CsharpCalculator.Webapp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
